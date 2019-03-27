74a01731c845e43f4879f94d8f62aacd95f1d583
Sergiusz Mazan <mazan.sergiusz@gmail.com>
2019-03-27 12:05:06 +0100

    Create basic toolbar

--- app/src/main/java/com/gmail/sergiusz/mazan/todolist/MainActivity.kt
+++ app/src/main/java/com/gmail/sergiusz/mazan/todolist/MainActivity.kt
@@ -2,11 +2,18 @@ package com.gmail.sergiusz.mazan.todolist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.main_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
